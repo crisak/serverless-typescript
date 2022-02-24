caac2f8e11c6f2137aea8916d8a32d617987c626
Cristian Romero <cristian.romero@softwareestrategico.com>
2022-02-23 20:23:44 -0500

    Config path

--- serverless.ts
+++ serverless.ts
@@ -4,29 +4,29 @@ import hello from '@functions/hello';
 import trackingService from '@functions/tracking-service';
 
 const serverlessConfiguration: AWS = {
-  service: 'tracking-functions',
-  frameworkVersion: '2',
-  custom: {
-    webpack: {
-      webpackConfig: './webpack.config.js',
-      includeModules: true
-    }
-  },
-  plugins: ['serverless-webpack'],
-  provider: {
-    name: 'aws',
-    runtime: 'nodejs14.x',
-    apiGateway: {
-      minimumCompressionSize: 1024,
-      shouldStartNameWithService: true
-    },
-    environment: {
-      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1'
-    },
-    lambdaHashingVersion: '20201221'
-  },
-  // import the function via paths
-  functions: { hello, 'tracking-service': trackingService }
+	service: 'tracking-functions',
+	frameworkVersion: '2',
+	custom: {
+		webpack: {
+			webpackConfig: './webpack.config.js',
+			includeModules: true
+		}
+	},
+	plugins: ['serverless-webpack'],
+	provider: {
+		name: 'aws',
+		runtime: 'nodejs14.x',
+		apiGateway: {
+			minimumCompressionSize: 1024,
+			shouldStartNameWithService: true
+		},
+		environment: {
+			AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1'
+		},
+		lambdaHashingVersion: '20201221'
+	},
+	// import the function via paths
+	functions: { hello, 'tracking-service': trackingService }
 };
 
 module.exports = serverlessConfiguration;
